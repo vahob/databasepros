bb5337eb0b93c1ca2d631ab150ddf47429520ac8
nate-stump <natestump2017@gmail.com>
2020-10-27 16:16:36 -0400

    Added more progress on task 4

--- team2-hw5-query.sql
+++ team2-hw5-query.sql
@@ -23,7 +23,6 @@ $$ LANGUAGE plpgsql;
 
 SELECT getCancellationTime(1);
 
-
 /* Task 3 */
 
 CREATE OR REPLACE FUNCTION isPlaneFull(flight_num INTEGER)
@@ -55,33 +54,65 @@ SELECT isPlaneFull(3);
 
 CREATE OR REPLACE PROCEDURE makeReservation
 (
-    cid INTEGER,
-    start_city CHAR(3),
-    destination_city CHAR(3)
+    reservation_number INTEGER,
+    flight_num INTEGER,
+    departure_date DATE,
+    leg INTEGER
+)
+AS $$
+    BEGIN
+        -- will need if statement to make sure the weekly schedule lines up
+        -- extracts the weekday (DOW) from departure date 
+        -- see if it lines up with the weekly schedule
+        -- 0-6, 0 is Sunday, 6 is Saturday
+        -- if instead of the cooresponding letter, there is a dash, raise an exception and/or exit the function
+        INSERT INTO Reservation_Detail VALUES
+        (
+            reservation_number, 
+            flight_num, 
+            to_TimeStamp(to_char(departure_date), 'YYYY-MM-DD'), 
+            leg
+        );
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE callMakeReservation
+(
+    flight_num INTEGER,
+    departure_date DATE,
+    leg INTEGER
 )
 AS $$
     DECLARE
-       reservation_date OurTimestamp.c_timestamp%TYPE;
-       temp_arrival_city CHAR(3);
+        reservation_number Reservation.reservation_number%TYPE;
+        departure_date Reservation.reservation_date%TYPE;
     BEGIN
-        WITH possible_starting_flights AS
-        (
-            SELECT *
-            FROM flight
-            WHERE departure_city = start_city;
-        ),
-        possible_ending_flights AS
-        (
-            SELECT *
-            FROM flight
-            WHERE arrival_city = destination_city;
-        )
-        WHILE destination_city != temp_arrival_city
-            LOOP
-                
-            END LOOP
-    COMMIT;
-    END
+        CALL makeReservation(reservation_number, flight_num, reservation_date, leg);
+    END;
 $$ LANGUAGE plpgsql;
 
-SELECT makeReservation();
+
+BEGIN;
+INSERT INTO Reservation VALUES(50,1,1160, '6859941825383380', (SELECT * FROM ourTimeStamp), TRUE);
+CALL callMakeReservation(7, '11-02-2020', 1);
+CALL callMakeReservation(8, '11-03-2020', 2);
+CALL callMakeReservation(9, '11-04-2020', 3);
+COMMIT;
+
+-- the departure date that you are given is just a date, not a timestamp
+-- the first thing you need to do is take the date and see if it lines up with the weekly schedule of the flight (day of week)
+-- if it does not, report this
+-- if it does line up, get the departure time from flights table
+-- take that string and concatonate it with the given string
+-- then convert to a timestamp and specify format
+
+-- make "callMakeReservation" wrapper function to declare variables for reservation_number and reservation_date, then use these as arguments for makeReservation
+
+-- first insert into reservation
+-- then use data from the most recently inserted reservation to call make reservation
+-- just manually call the makeReservation the proper number of times based off of how many legs there are
+-- functionality for doing that automatically will be added later
+
+-- substr is 1-indexed - first character is 1
+
+-- can type rollback to get rid of aborted function
