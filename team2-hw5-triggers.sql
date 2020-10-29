88975f356df439f4c8f02841513d6a6b27eec723
nate-stump <natestump2017@gmail.com>
2020-10-29 12:15:04 -0400

    Progress on Task 6

--- team2-hw5-triggers.sql
+++ team2-hw5-triggers.sql
@@ -46,14 +46,41 @@ CREATE OR REPLACE FUNCTION remove_reservation()
 RETURNS TRIGGER
 AS $$
     DECLARE
-    
+        cancellationTime Timestamp;
     BEGIN
-    
+        cancellationTime = getCancellationTime();
+        SELECT INTO cancellation_time_flights
+        FROM Flight
+        WHERE OurTimestamp.c_timestamp <= getCancellationTime(flight_number);
+        
+        -- join cancellation_time_flights with reservation details table
+        
+        -- THEN join with Reservation where ticketed = FALSE
+        
+        -- that will give you all the rows where it is non-ticketed and needs to be canceled
+        -- cancel every row in this table
+        
+        -- should only need a single loop
+        
+        -- delete where reservation number is in ... query result
+        
+        FOR ROW IN cancellation_time_flights LOOP
+            FOR ROW IN Reservation LOOP
+                DELETE FROM Reservation
+                WHERE Reservation.ticketed = FALSE AND Reservation.;
+                -- remove non-ticketed reservations from each of those flights
+                -- Delete from reservation_detail as well?
+            END LOOP;
+        END LOOP;
+        -- downsize flights
     END
 $$ LANGUAGE plpgsql;
 
+-- This part is complete - any time OurTimestamp is updated, you just generally call remove_reservation once and handle all flights in one call
 CREATE TRIGGER cancelReservation
-
+AFTER INSERT OR UPDATE ON OurTimestamp
+FOR EACH ROW
 EXECUTE FUNCTION remove_reservation();
 
-
+-- Cancellation time is the time at which unpaid (ticketed = false) are removed from the table
+-- It has nothing to do with the flight itself being cancelled
