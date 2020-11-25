8f9c9ab00089df96ed2e9da0e9035cb628e6c2dc
nate-stump <natestump2017@gmail.com>
2020-11-25 00:48:12 -0500

    Completed report

--- Interface.java
+++ Interface.java
@@ -1,3 +1,9 @@
+/* Team 2 */
+/* DatabasePros */
+/* Vokhob Bazorov - vkb4@pitt.edu */
+/* Luiza Urazaeva - lau4@pitt.edu */
+/* Nathaniel Stump - nrs70@pitt.edu */
+
 import java.util.*;
 import java.io.*;
 import java.sql.*;
